759f61789e361ec5556860262c47d3a200cc612a
sakshamkapoorspr <saksham.kapoor@sprinklr.com>
2022-06-28 22:50:18 +0530

    set up sentry monitoring

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import * as Sentry from "@sentry/react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        onClick={() => {
+          throw Error("Some React Error!");
+        }}
+      >
+        Break the world
+      </button>
+      <button
+        onClick={() => {
+          throw TypeError("Invalid Type");
+        }}
+      >
+        Type Error
+      </button>
+      <button
+        onClick={() => {
+          throw RangeError("Invalid Range");
+        }}
+      >
+        Range Error
+      </button>
+      <button
+        onClick={() => {
+          throw Error("Some New Error");
+        }}
+      >
+        Range Error
+      </button>
     </div>
   );
 }
 
-export default App;
+export default Sentry.withProfiler(App);
--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+Sentry.init({
+  dsn: "https://0a80bd11489b443facc19994e6f13917@o1300671.ingest.sentry.io/6535569",
+  integrations: [new BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
